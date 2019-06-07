b1800f1cef4033b351cc5a1a7ede4fb11ee6505f
Haktan Yucesoy <38834423+Hacktin@users.noreply.github.com>
2019-06-07 12:11:40 +0300

    Add files via upload

---
+++ Siparis.DataAccessTest/DataAccessTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Siparis.DataAccess.Concart.Entity_Framework;
+using System.Data.Entity;
+using Siparis.Core;
+
+
+namespace Siparis.DataAccessTest
+{
+    [TestClass]
+    public class DataAccessTest
+    {
+        [TestMethod]
+        public void GetAllCustomers()
+        {
+            EntityFrameworkMusteriDAL musteri_dal = new EntityFrameworkMusteriDAL();
+
+            var result= musteri_dal.GetAll();
+
+            Assert.AreEqual(4, result.Count);
+
+        }
+
+
+        [TestMethod]
+        public void GetAllPhones()
+        {
+            EntityFrameworkTelefonDAL telefon_dal = new EntityFrameworkTelefonDAL();
+
+            var result = telefon_dal.GetAll();
+
+            Assert.AreEqual(4, result.Count);
+        }
+
+      
+
+
+    }
+}
---
+++ Siparis.DataAccessTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Siparis.DataAccessTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Siparis.DataAccessTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4460bdcf-6294-4efa-9f7a-d18ab6d4a125")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
