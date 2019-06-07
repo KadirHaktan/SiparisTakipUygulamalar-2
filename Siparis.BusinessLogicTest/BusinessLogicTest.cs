66ec30892671f669942d24d304c695cfbb864b47
Haktan Yucesoy <38834423+Hacktin@users.noreply.github.com>
2019-06-07 12:22:18 +0300

    Add files via upload

---
+++ Siparis.BusinessLogicTest/BusinessLogicTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Siparis.BusinessLogic.Concart;
+using Moq;
+using Siparis.DataAccess.Abstract;
+using Siparis.Entity;
+using FluentValidation;
+using System.Collections.Generic;
+
+namespace Siparis.BusinessLogicTest
+{
+    [TestClass]
+    public class BusinessLogicTest
+    {
+       
+
+     
+    
+
+        [TestMethod]
+        public void SiparisManagerTest1()
+        {
+            Mock<ISiparisDAL> siparis_dal = new Mock<ISiparisDAL>();
+
+            SiparisManager siparis_manager = new SiparisManager(siparis_dal.Object);
+
+            siparis_manager.Add(new _Siparis
+            {
+                Urun_ID = 1,
+                Musteri_ID = 1003
+            });
+
+        }
+
+        [TestMethod]
+        public void MusteriManagerTest3()
+        {
+            Mock<IMusteriDAL> dal = new Mock<IMusteriDAL>();
+            Mock<ITelefonDAL> dal1 = new Mock<ITelefonDAL>();
+
+            MusteriManager manager = new MusteriManager(dal.Object);
+
+            var result=manager.GetAllCustomers();
+
+            Assert.AreEqual(null, result);
+        }
+
+    }
+}
---
+++ Siparis.BusinessLogicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Siparis.BusinessLogicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Siparis.BusinessLogicTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8d5ced7-0caf-452a-8222-7140ecba16a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
